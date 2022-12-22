b586a5f3cc0ccc48570086dea37fb18cf85805fa
Ricky Tran <diabloxtran@gmail.com>
2022-12-21 22:23:52 -0500

    added index method and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_014102) do
+  create_table "museums", force: :cascade do |t|
+    t.string "postcode"
+    t.string "place_name"
+    t.float "longitude"
+    t.float "latitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
